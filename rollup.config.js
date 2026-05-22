2da52bdb084b34e3acafbcb93ec6c80022918faf
Jrjuan7704 <Jrjuan7704@gmail.com>
2026-05-22 15:01:37 -0700

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,23 @@
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+
 export default {
-  input: "src/main.jsx",
-  output: {
-    dir: "dist",
-    format: "es",
-  },
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.mjs',
+      format: 'esm',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.cjs',
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    typescript({ tsconfig: './tsconfig.json' }),
+    terser(),
+  ],
 };
